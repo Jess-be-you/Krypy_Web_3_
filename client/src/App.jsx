389cae7418b72b8f9e857f48837758a12498699c
Jess-be-you <jesmipj001@gmail.com>
2023-06-10 16:37:55 +0530

    changed app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 const App=()=> {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
+      <h1 className="text-3xl font-bold">
+        JESMI PJ
       </h1>
     </>
   )
